f1ee29bfbc205bb8b02a09bab91453e705514f11
WeijianShi <shiweijian0818@gmail.com>
2023-04-12 10:56:18 -0400

    transfer to py

---
+++ Hashing.py
@@ -0,0 +1,42 @@
+import args
+
+
+# create N_gram vector given a word
+def n_gram(word, n=args.N):
+    s = []
+    word = '#' + word + '#'
+    for j in range(len(word) - 2):
+        s.append(word[j:j + 3])  # letter 3_gram
+    return s
+
+
+# create list of words in a sentence
+def lst_gram(lst, n=args.N):
+    slice = []
+    for word in str(lst).lower().split():
+        slice.extend(n_gram(word))
+    return slice
+
+
+vocab = []  # initiate a vector to store letter n_grams
+file_path = '/Users/wesley/Desktop/4579_final/MRPC/'  # data set path
+files = ['train_data.csv', 'test_data.csv']  # training data & testing data set
+
+for file in files:
+    f = open(file_path + file, encoding='utf-8').readlines()
+    for i in range(1, len(f)):  # line 0 is header
+        s1, s2 = f[i][2:].strip('\n').split('\t')
+        # delete the first 3 character of each line, tab space, and line change space, parse into two sentences
+        vocab.extend(lst_gram(s1))
+        vocab.extend(lst_gram(s2))
+
+vocab = set(vocab)
+vocab_list = ['[PAD]', '[UNK]']
+vocab_list.extend(list(vocab))
+
+print(1)
+vocab_file = args.VOCAB_FILE
+with open(vocab_file, 'w', encoding='utf-8') as f:
+    for sl in vocab_list:
+        f.write(sl)
+        f.write('\n')
---
+++ args.py
@@ -0,0 +1,15 @@
+# defining variables and reference files needed
+TRAIN_DATA = '/Users/wesley/Desktop/4579_final/MRPC/train_data.csv'
+TEST_DATA = '/Users/wesley/Desktop/4579_final/MRPC/test_data.csv'
+
+VOCAB_FILE = '/Users/wesley/Desktop/4579_final/vocab_mrpc.txt'
+MODEL_FILE = './dssm_pr.pkl'
+
+N = 3
+SENTENCE_MAXLEN = 90
+CHAR_SIZE = 10041
+EMBEDDING_SIZE = 300
+
+EPOCH = 100
+BATCH_SIZE = 50
+LR = 0.0005
