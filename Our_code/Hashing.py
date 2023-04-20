6355f46e84a0d19c09bf338a5a6399f7621a1853
weijianshi <shiweijian0818@gmail.com>
2023-04-19 23:43:17 -0400

    update

--- Our_code/Hashing.py
+++ Our_code/Hashing.py
@@ -19,16 +19,20 @@ def lst_gram(lst, n=args.N):
 
 
 vocab = []  # initiate a vector to store letter n_grams
-file_path = '/Users/wesley/Desktop/4579_final/MRPC/'  # data set path
-files = ['train_data.csv', 'test_data.csv']  # training data & testing data set
+file_path = '/Users/wesley/Documents/GitHub/IEOR4579_Final_Project/Data/'  # data set path
+files = ['questions_train.tsv']  # training data & testing data set
 
 for file in files:
     f = open(file_path + file, encoding='utf-8').readlines()
-    for i in range(1, len(f)):  # line 0 is header
-        s1, s2 = f[i][2:].strip('\n').split('\t')
-        # delete the first 3 character of each line, tab space, and line change space, parse into two sentences
-        vocab.extend(lst_gram(s1))
-        vocab.extend(lst_gram(s2))
+    try:
+        for i in range(1, len(f)):  # line 0 is header
+            s1, s2 = f[i][2:].strip('\n').split('\t')
+            # delete the first 3 character of each line, tab space, and line change space, parse into two sentences
+            vocab.extend(lst_gram(s1))
+            vocab.extend(lst_gram(s2))
+    except:
+        print(i)
+        print(f[i])
 
 vocab = set(vocab)  # clean list to be a non_duplicate set
 vocab_list = ['[PAD]', '[UNK]']
